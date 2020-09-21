df62a5905770d4889d224d6e84c347feb7cac09a
Konstantim <const.trofimov@gmail.com>
2020-09-21 12:12:49 +0300

    v4.3

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const router = express.Router()
 const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer')
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const app = express()
 
-const PORT = process.env.PORT || 3030
-
-let data = undefined
 
 
 app.use(express.static(path.join(__dirname, 'static')))
+app.use('/', router)
 app.use('/', bodyParser.urlencoded({
     extended: true
 }));
-app.use('/', router)
 
-app.use((req, res, next) => {
-    if (res.status(400)) {
-        res.sendFile(path.join(__dirname + '/static/views/error.html'));
-    }
-})
 
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something went wrong...')
-})
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/static/index.html'));
@@ -34,6 +22,34 @@ router.get('/', (req, res) => {
 router.get('/policy', (req, res) => {
     res.sendFile(path.join(__dirname + '/static/views/policy.html'));
 });
+
+
+
+app.post('/', function (req, res) {
+    const message = {
+        subject: 'Message from Portfolio',
+        html:
+            ` 
+            <p>Имя: <strong>${req.body.name}</strong></p> 
+            <p>Email: <strong>${req.body.email}</strong></p> 
+            <hr>
+            <p>${req.body.text}</p>
+        `
+    }
+    mailer(message)
+    res.redirect('/')
+});
+
+
+
+app.use(function (req, res) {
+    res.status(404).sendFile(path.join(__dirname + '/static/views/error.html'));
+
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send('Something went wrong...')
+})
 
 
 
@@ -53,22 +69,6 @@ const transporter = nodemailer.createTransport(
     }
 )
 
-app.post('/', function (req, res, next) {
-    data = req.body
-    const message = {
-        subject: 'Message from Portfolio',
-        html:
-            ` 
-            <p>Имя: <strong>${req.body.name}</strong></p> 
-            <p>Email: <strong>${req.body.email}</strong></p> 
-            <hr>
-            <p>${req.body.text}</p>
-        `
-    }
-    mailer(message)
-    res.redirect('/')
-});
-
 const mailer = message => {
     transporter.sendMail(message, (err, info) => {
         if (err) console.log(err)
@@ -77,6 +77,8 @@ const mailer = message => {
 }
 
 
+
+const PORT = process.env.PORT || 3030
 
 app.listen(PORT, () => {
     console.log(__dirname)
